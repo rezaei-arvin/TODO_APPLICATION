1a60bef451ec23ac887d2625ea4a8648341d024d
rezaei-arvin <rezaei_arvin@yahoo.com>
2022-01-23 13:06:33 +0330

    Create Todo

---
+++ MyApplication/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Models
+{
+	public class TodoItem : object
+	{
+		public TodoItem() : base()
+		{
+		}
+
+		public bool IsDone { get; set; }
+
+		public string Title { get; set; }
+	}
+}
